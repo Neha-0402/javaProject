4ba79ded02d7258034f12b3f5e42de883d53122b
Neha Gupta <120816380+Neha-0402@users.noreply.github.com>
2023-05-10 16:25:52 +0530

    Update AssingmentCurdApiApplication.java

--- AssingmentCurdApiApplication.java
+++ AssingmentCurdApiApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AssingmentCurdApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AssingmentCurdApiApplication.class, args);
+		SpringApplication.run(AssingmentCurdApiApplication.class,args);
 	}
 
 }
